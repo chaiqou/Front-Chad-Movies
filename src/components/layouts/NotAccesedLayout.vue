7576a6881f59ec2d9256d8313f2d7fc3d6c44b79
chaiqou <lomtadzenikusha@gmail.com>
2022-06-28 01:20:09 +0400

    feat: Create layout for 404 and forbidden pages

---
+++ src/components/layouts/NotAccesedLayout.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="flex flex-col items-center justify-center h-screen w-screen">
+    <slot></slot>
+    <h1 class="text-white text-center font-bold text-5xl mt-6">
+      <slot name="major"></slot>
+    </h1>
+    <h5 class="text-white text-center font-medium text-2xl mt-6">
+      <slot name="minor"></slot>
+    </h5>
+    <RouterLink
+      to="/"
+      class="inline-flex items-center px-4 py-2 mt-4 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-[#E31221] focus:outline-none focus:ring-2 focus:ring-offset-2"
+    >
+      Return home
+    </RouterLink>
+  </div>
+</template>
+
+<script>
+import { RouterLink } from "vue-router";
+export default {
+  components: {
+    RouterLink,
+  },
+};
+</script>
