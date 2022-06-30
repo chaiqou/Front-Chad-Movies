1dc053028675d21663d90790726358b2b051bf6b
chaiqou <lomtadzenikusha@gmail.com>
2022-07-01 02:56:44 +0400

    chore: add mini footer to landing page

--- src/components/landing/LandingScrollableImages.vue
+++ src/components/landing/LandingScrollableImages.vue
@@ -30,7 +30,7 @@
     </div>
   </section>
   <section
-    class="h-[100vh] text-white bg-shutter-island bg-no-repeat bg-cover bg-center snap-start"
+    class="h-[99vh] text-white bg-shutter-island bg-no-repeat bg-cover bg-center snap-start"
   >
     <div class="flex items-center justify-left h-screen ml-6 md:ml-12">
       <img
@@ -42,5 +42,8 @@
         "Which Would Be Worse: To Live As A Monster, Or To Die As A Good Man?"
       </h1>
     </div>
+    <p class="text-[#DDCCAA] font-normal ml-6">
+      © 2022 movie quotes. All rights reserved.
+    </p>
   </section>
 </template>
