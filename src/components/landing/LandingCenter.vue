0f4e70e137b81e2cf861d7ca1edeefdbcb3d0500
chaiqou <lomtadzenikusha@gmail.com>
2022-07-01 15:41:26 +0400

    style: fix landing page styles and image resolutions

--- src/components/landing/LandingCenter.vue
+++ src/components/landing/LandingCenter.vue
@@ -7,14 +7,14 @@
         Find any quote in
       </p>
       <p
-        class="text-[#DDCCAA] font-bold text-2xl md:text-6xl leading-normal mb-8"
+        class="text-[#DDCCAA] font-bold text-2xl md:text-6xl leading-normal mb-8 md:mt-4"
       >
         millions of movie lines
       </p>
       <router-link
         to="/register"
         class="text-white bg-[#E31221] hover:bg-red-800 font-medium rounded-lg text-sm px-5 py-2.5 mr-2"
-        >Sign Up</router-link
+        >Get started</router-link
       >
     </div>
   </div>
--- src/components/landing/LandingScrollableImages.vue
+++ src/components/landing/LandingScrollableImages.vue
@@ -4,13 +4,16 @@
   >
     <div class="flex items-center justify-left h-screen ml-6 md:ml-12">
       <img
-        class="md:mb-12 w-1/12 md:w-12 mb-8 mr-4"
+        class="md:mb-28 w-1/12 md:w-12 mr-4 mb-24"
         src="@/assets/svgs/line.svg"
         alt="line"
       />
       <h1 class="font-bold md:text-5xl text-2xl md:w-1/2 w-2/3">
-        “You have to leave somethig <br />
-        behind to go forward”
+        “You have to leave somethig behind to go forward”
+        <br />
+        <span class="text-base md:text-3xl font-bold leading-0 text-[#D9D9D9]"
+          >Interstellar, 2014</span
+        >
       </h1>
     </div>
   </section>
@@ -19,13 +22,17 @@
   >
     <div class="flex items-center justify-left h-screen ml-6 md:ml-12">
       <img
-        class="md:mb-12 w-1/12 md:w-12 mb-8 mr-4"
+        class="md:mb-30 w-1/12 md:w-12 mr-4 mb-40"
         src="@/assets/svgs/line.svg"
         alt="line"
       />
       <h1 class="font-bold md:text-5xl text-2xl md:w-1/2 w-2/3">
         I think we’re just gonna have to be secretly in love with earch other
         and leave it that
+        <br />
+        <span class="text-base md:text-3xl font-bold leading-0 text-[#D9D9D9]"
+          >The Royal Tenenbaums,2001
+        </span>
       </h1>
     </div>
   </section>
@@ -34,12 +41,17 @@
   >
     <div class="flex items-center justify-left h-screen ml-6 md:ml-12">
       <img
-        class="md:mb-12 w-1/12 md:w-12 mb-8 mr-4"
+        class="md:mb-30 w-1/12 md:w-12 mr-4 mb-40"
         src="@/assets/svgs/line.svg"
         alt="line"
       />
       <h1 class="font-bold md:text-5xl text-2xl md:w-1/2 w-2/3">
-        "Which Would Be Worse: To Live As A Monster, Or To Die As A Good Man?"
+        I think we’re just gonna have to be secretly in love with earch other
+        and leave it that
+        <br />
+        <span class="text-base md:text-3xl font-bold leading-0 text-[#D9D9D9]"
+          >The Lord of the Rings, 2003</span
+        >
       </h1>
     </div>
     <p class="text-[#DDCCAA] font-normal ml-6">
