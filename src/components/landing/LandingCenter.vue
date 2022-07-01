211f39451beddf9618a93c1f2658acc79f32bf27
chaiqou <lomtadzenikusha@gmail.com>
2022-07-01 12:43:09 +0400

    fix: Landing page responsive styles

--- src/components/landing/LandingCenter.vue
+++ src/components/landing/LandingCenter.vue
@@ -1,14 +1,21 @@
 <template>
-  <div class="flex items-center justify-center relative h-[70vh]">
-    <h1
-      class="text-[#DDCCAA] md:w-1/3 text-center leading-9 font-bold md:text-6xl text-4xl whitespace-wrap"
-    >
-      Find any quote in millions of movie lines
-    </h1>
-    <router-link
-      to="/register"
-      class="absolute top-[42vh] right-[18 vh] md:top-[46vh] md:right-[92vh] focus:outline-none text-white bg-[#E31221] hover:bg-red-800 focus:ring-4 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2"
-      >Get Started</router-link
-    >
+  <div
+    class="flex flex-wrap content-center items-center justify-center h-screen text-center"
+  >
+    <div>
+      <p class="text-[#DDCCAA] font-bold text-2xl md:text-6xl leading-normal">
+        Find any quote in
+      </p>
+      <p
+        class="text-[#DDCCAA] font-bold text-2xl md:text-6xl leading-normal mb-8"
+      >
+        millions of movie lines
+      </p>
+      <router-link
+        to="/register"
+        class="text-white bg-[#E31221] hover:bg-red-800 font-medium rounded-lg text-sm px-5 py-2.5 mr-2"
+        >Sign Up</router-link
+      >
+    </div>
   </div>
 </template>
