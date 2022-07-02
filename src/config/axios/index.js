0ed89e72247d841afa03d179da0369ac0471f284
chaiqou <lomtadzenikusha@gmail.com>
2022-07-02 23:22:01 +0400

    feat: save token inside cookies

--- src/config/axios/index.js
+++ src/config/axios/index.js
@@ -1,14 +1,15 @@
 import axios from "axios";
+import { getJwtToken } from "@/jwt/index.js";
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
+  baseURL: import.meta.env.LARAVE_API_LOCAL_URL,
   timeout: 1000,
   headers: {
     Accept: "application/json",
     "Content-Type": "application/json",
     "Access-Control-Allow-Origin": "*",
     "X-Requested-With": "XMLHttpRequest",
-    Authorization: "Bearer ",
+    Authorization: "Bearer " + getJwtToken(),
   },
 });
 
