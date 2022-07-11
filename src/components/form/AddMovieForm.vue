a206a97832b7681e5c967d796f2cda4fc0b0e687
chaiqou <lomtadzenikusha@gmail.com>
2022-07-11 10:19:41 +0400

    refactor: create seperate component for add movie form modal

---
+++ src/components/form/AddMovieForm.vue
@@ -0,0 +1,22 @@
+<template>
+  <FormVee class="mt-10 min-w-screen space-y-8">
+    <MovieInput name="name" placeholder="Movie name" />
+    <MovieInput name="სახელი" placeholder="ფილმის სახელი" />
+    <MovieInput name="director" placeholder="Director" />
+    <MovieInput name="რეჟისორი" placeholder="რეჟისორი" />
+    <MovieInput name="description" placeholder="Movie Description" />
+    <MovieInput name="აღწერა" placeholder="ფილმის აღწერა" />
+  </FormVee>
+</template>
+
+<script>
+import { Form as FormVee } from "vee-validate";
+import MovieInput from "./MovieInput.vue";
+
+export default {
+  components: {
+    FormVee,
+    MovieInput,
+  },
+};
+</script>
