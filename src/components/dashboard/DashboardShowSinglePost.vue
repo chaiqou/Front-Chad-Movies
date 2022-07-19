8cc58019f490ca2fe900a5cace0fcf1bee890697
chaiqou <lomtadzenikusha@gmail.com>
2022-07-19 09:43:43 +0400

    feat: show individual post with data

---
+++ src/components/dashboard/DashboardShowSinglePost.vue
@@ -0,0 +1,68 @@
+<template>
+  <DashboardTimeline>
+    <div
+      v-if="post"
+      class="bg-[#11101A] rounded-lg w-2/3 mt-6 ml-48 overflow-hidden"
+    >
+      <div class="flex flex-col p-4">
+        <div class="flex items-center">
+          <div class="w-8">AQ PROFILE IMAGE</div>
+          <div class="ml-6">
+            <div class="text-sm text-white font-bold">
+              {{ post.userinfo.name }}
+            </div>
+          </div>
+        </div>
+        <div class="mt-4">
+          <p class="text-white font-bold text-sm">
+            {{ post.quote_ka }}
+            <span class="text-[#DDCCAA]">{{ post.movieinfo.title.en }}</span>
+          </p>
+        </div>
+      </div>
+      <div class="w-full p-4">
+        <img
+          :src="backend + post.movieinfo.thumbnail"
+          alt="postimage"
+          class="w-full rounded-md"
+        />
+      </div>
+      <div class="flex border-1 border-green-500 m-4 space-x-4">
+        <button class="flex justify-center py-2">
+          <p class="mr-2 text-white font-bold">10</p>
+          <IconDashboardComment />
+        </button>
+        <button class="flex justify-center py-2 rounded-lg">
+          <p class="mr-2 text-white font-bold">20</p>
+          <IconDashboardHearth />
+        </button>
+      </div>
+      <div>
+        <div>
+          <p class="text-white">aq komentarebis</p>
+        </div>
+      </div>
+    </div>
+  </DashboardTimeline>
+</template>
+
+<script>
+import IconDashboardComment from "@/components/icons/IconDashboardComment.vue";
+import IconDashboardHearth from "@/components/icons/IconDashboardHearth.vue";
+import DashboardTimeline from "@/components/dashboard/DashboardTimeline.vue";
+
+export default {
+  components: { IconDashboardComment, IconDashboardHearth, DashboardTimeline },
+
+  props: {
+    post: {
+      type: Object,
+      required: true,
+    },
+    backend: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
--- src/views/quotes/QuoteIndividual.vue
+++ src/views/quotes/QuoteIndividual.vue
@@ -1,20 +1,32 @@
 <template>
-  <div>gg</div>
+  <DashboardShowSinglePost
+    :post="individual_quote"
+    :backend="backurl"
+  ></DashboardShowSinglePost>
 </template>
 
 <script>
+import DashboardShowSinglePost from "@/components/dashboard/DashboardShowSinglePost.vue";
 import axios from "@/config/axios/index";
 import { mapWritableState } from "pinia";
 import { useAddQuoteStore } from "@/stores/useAddQuoteStore";
+import { useMovieListStore } from "@/stores/useMovieListStore";
+
 export default {
+  components: { DashboardShowSinglePost },
   computed: {
     ...mapWritableState(useAddQuoteStore, ["individual_quote"]),
+    ...mapWritableState(useMovieListStore, ["backurl"]),
   },
   created() {
-    console.log(this.individual_quote);
-    axios.get(`/quotes/${this.$route.params.id}`).then((response) => {
-      this.individual_quote = response.data.data;
-    });
+    axios
+      .get(`/quotes/${this.$route.params.id}`)
+      .then((response) => {
+        this.individual_quote = response.data.data;
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   },
 };
 </script>
