b88d574c6c9333d6eb94a8c7ac17658acaafa915
chaiqou <lomtadzenikusha@gmail.com>
2022-07-20 16:52:18 +0400

    refactor: Divide seperate Notification Read and Notification Unread components

--- src/components/dashboard/DashboardNotificationModal.vue
+++ src/components/dashboard/DashboardNotificationModal.vue
@@ -28,110 +28,25 @@
           </div>
         </div>
         <!-- NOTIFIKACIEBI IWYEBA AQEDAN -->
-        <div
-          v-for="notification in unread"
-          :key="notification.id"
-          class="relative grid gap-6 bg-[#000000] px-5 py-6 sm:gap-8 sm:p-8"
-        >
-          <button
-            class="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-900 transition ease-in-out duration-150"
-            @click="readNotification(notification)"
-          >
-            <img
-              src="https://i.ibb.co/C1sj76g/Screenshot-from-2022-07-18-22-07-37.png"
-              alt="profildis foto"
-              class="w-8 h-8 object-cover rounded-full"
-            />
-            <div class="flex items-center justify-between">
-              <div class="ml-4">
-                <div
-                  v-if="notification.data.hasOwnProperty('commentBy')"
-                  class="mt-1 text-sm text-white flex"
-                >
-                  <p class="text-base font-medium text-white">
-                    {{ notification.data.commentBy }}
-                  </p>
-                  <IconComment />
-                  <span class="ml-2">Commented to your movie quote</span>
-                </div>
-                <div v-else class="mt-1 text-sm text-white flex">
-                  <p class="text-base font-medium text-white">
-                    {{ notification.data.likedBy }}
-                  </p>
-                  <IconSmallHearth />
-                  <span class="ml-2">Reacted to your quote</span>
-                </div>
-              </div>
-              <div>
-                <p class="text-base font-medium text-white ml-10">
-                  {{ notification.data.created_at }}
-                </p>
-                <p class="mt-1 text-sm text-[#198754] flex">
-                  <span class="ml-12">New</span>
-                </p>
-              </div>
-            </div>
-          </button>
-        </div>
-        <!-- WAKITXULI NOTIFICATIONS -->
-        <div
-          v-for="notification in read"
-          :key="notification.id"
-          class="relative grid gap-6 bg-[#000000] px-5 py-6 sm:gap-8 sm:p-8"
-        >
-          <button
-            class="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-900 transition ease-in-out duration-150"
-            z
-          >
-            <img
-              src="https://i.ibb.co/C1sj76g/Screenshot-from-2022-07-18-22-07-37.png"
-              alt="profildis foto"
-              class="w-8 h-8 object-cover rounded-full"
-            />
-            <div class="flex items-center justify-between">
-              <div class="ml-4">
-                <div
-                  v-if="notification.data.hasOwnProperty('commentBy')"
-                  class="mt-1 text-sm text-white flex"
-                >
-                  <p class="text-base font-medium text-white">
-                    {{ notification.data.commentBy }}
-                  </p>
-                  <IconComment />
-                  <span class="ml-2">Commented to your movie quote</span>
-                </div>
-                <div v-else class="mt-1 text-sm text-white flex">
-                  <p class="text-base font-medium text-white">
-                    {{ notification.data.likedBy }}
-                  </p>
-                  <IconSmallHearth />
-                  <span class="ml-2">Reacted to your quote</span>
-                </div>
-              </div>
-              <div>
-                <p class="text-base font-medium text-white ml-10 mb-6">
-                  {{ notification.data.created_at }}
-                </p>
-              </div>
-            </div>
-          </button>
-        </div>
-
-        <!-- WAKITXULI NOTIFICATIONS -->
+        <NotificationUnread :unreaddata="unread" />
+        <NotificationRead :readdata="read" />
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import IconComment from "@/components/icons/IconComment.vue";
 import { mapWritableState } from "pinia";
 import { useNotificationsStore } from "@/stores/useNotificationsStore";
 import axios from "@/config/axios/index";
-import IconSmallHearth from "../icons/IconSmallHearth.vue";
+import NotificationUnread from "@/components/notifications/NotificationUnread.vue";
+import NotificationRead from "@/components/notifications/NotificationRead.vue";
 
 export default {
-  components: { IconComment, IconSmallHearth },
+  components: {
+    NotificationUnread,
+    NotificationRead,
+  },
   computed: {
     ...mapWritableState(useNotificationsStore, [
       "read",
@@ -141,17 +56,6 @@ export default {
   },
 
   methods: {
-    readNotification(notification) {
-      axios
-        .post("markAsRead", {
-          id: notification.id,
-        })
-        .then(() => {
-          this.unread.splice(notification, 1);
-          this.read.push(notification);
-          this.unreadCount--;
-        });
-    },
     readAllNotification() {
       axios.post("markAllAsRead").then(() => {
         this.read.concat(this.unread);
---
+++ src/components/notifications/NotificationRead.vue
@@ -0,0 +1,85 @@
+<template>
+  <div
+    v-for="notification in readdata"
+    :key="notification.id"
+    class="relative grid gap-6 bg-[#000000] px-5 py-6 sm:gap-8 sm:p-8"
+  >
+    <button
+      class="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-900 transition ease-in-out duration-150"
+      z
+    >
+      <img
+        src="https://i.ibb.co/C1sj76g/Screenshot-from-2022-07-18-22-07-37.png"
+        alt="profildis foto"
+        class="w-8 h-8 object-cover rounded-full"
+      />
+      <div class="flex items-center justify-between">
+        <div class="ml-4">
+          <div
+            v-if="notification.data.hasOwnProperty('commentBy')"
+            class="mt-1 text-sm text-white flex"
+          >
+            <p class="text-base font-medium text-white">
+              {{ notification.data.commentBy }}
+            </p>
+            <IconComment />
+            <span class="ml-2">Commented to your movie quote</span>
+          </div>
+          <div v-else class="mt-1 text-sm text-white flex">
+            <p class="text-base font-medium text-white">
+              {{ notification.data.likedBy }}
+            </p>
+            <IconSmallHearth />
+            <span class="ml-2">Reacted to your quote</span>
+          </div>
+        </div>
+        <div>
+          <p class="text-base font-medium text-white ml-10 mb-6">
+            {{ notification.data.created_at }}
+          </p>
+        </div>
+      </div>
+    </button>
+  </div>
+</template>
+<script>
+import { mapWritableState } from "pinia";
+import { useNotificationsStore } from "@/stores/useNotificationsStore";
+import axios from "@/config/axios/index";
+import IconSmallHearth from "../icons/IconSmallHearth.vue";
+import IconComment from "../icons/IconComment.vue";
+
+export default {
+  components: {
+    IconComment,
+    IconSmallHearth,
+  },
+  props: {
+    readdata: {
+      type: Object,
+      required: true,
+    },
+  },
+  computed: {
+    ...mapWritableState(useNotificationsStore, [
+      "read",
+      "unread",
+      "unreadCount",
+    ]),
+  },
+
+  methods: {
+    readNotification(notification) {
+      axios
+        .post("markAsRead", {
+          id: notification.id,
+        })
+        .then(() => {
+          this.unread.splice(notification, 1);
+          this.read.push(notification);
+          this.unreadCount--;
+        });
+    },
+  },
+};
+</script>
