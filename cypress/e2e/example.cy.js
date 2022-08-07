6b8d8e7fb3d0073a8e8c58ddc939a13083f2977a
chaiqou <lomtadzenikusha@gmail.com>
2022-08-07 15:39:47 +0400

    chore: configure cypress

--- cypress/e2e/example.cy.js
+++ cypress/e2e/example.cy.js
@@ -1,8 +1,8 @@
-// https://docs.cypress.io/api/introduction/api.html
+<reference types="cypress" />;
 
 describe("My First Test", () => {
   it("visits the app root url", () => {
     cy.visit("/");
-    cy.contains("h1", "You did it!");
+    cy.contains("p", "Find any quote");
   });
 });
