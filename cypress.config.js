72390f2be12f54ba1a47aa75b639f5fc4dc9f155
chaiqou <lomtadzenikusha@gmail.com>
2022-08-08 14:41:39 +0400

    chore: remove cypress example tests and fix testing viewport

--- cypress.config.js
+++ cypress.config.js
@@ -4,7 +4,7 @@ module.exports = defineConfig({
   e2e: {
     specPattern: "cypress/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}",
     baseUrl: "http://localhost:3000",
-    viewportHeight: 1920,
-    viewportWidth: 1080,
+    viewportHeight: 1080,
+    viewportWidth: 1920,
   },
 });
