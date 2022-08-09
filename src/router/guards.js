76eeb422324771a500c312d4e25cfa24b1af765b
chaiqou <lomtadzenikusha@gmail.com>
2022-08-09 09:41:01 +0400

    fix: custom guard

--- src/router/guards.js
+++ src/router/guards.js
@@ -1,2 +1,5 @@
-export const userIsAuthenticated = () =>
-  localStorage.getItem("jwt_token") === null ? "/unauthorized" : "";
+export const userIsAuthenticated = () => {
+  if (!localStorage.getItem("jwt_token")) {
+    return "/unauthorized";
+  }
+};
