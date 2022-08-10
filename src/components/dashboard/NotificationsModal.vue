bb24690886943538d91a9c120d64354e02d10b2c
chaiqou <lomtadzenikusha@gmail.com>
2022-08-10 19:46:36 +0400

    refactor: close notifications model if outside clicked * convert notification component from options api to composition api

--- src/components/dashboard/NotificationsModal.vue
+++ src/components/dashboard/NotificationsModal.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="mt-10 flex">
+  <div v-if="notificationRef" ref="notificationRef" class="mt-10 flex">
     <div
       class="fixed md:absolute z-10 left-[55vw] sm:left-[60vw] md:left-1/3 transform -translate-x-60 mt-3 w-screen max-w-xl"
     >
@@ -21,48 +21,40 @@
           <div class="flex">
             <button
               class="-m-3 p-3 flex items-center rounded-md text-base font-medium text-white"
-              @click="readAllNotification"
+              @click="readAllNotification()"
             >
               <span class="ml-3 mb-4">Mark as all read</span>
             </button>
           </div>
         </div>
-        <NotificationUnread :unreaddata="unread" />
-        <NotificationRead :readdata="read" />
+        <NotificationUnread :unreaddata="notification.unread" />
+        <NotificationRead :readdata="notification.read" />
       </div>
     </div>
   </div>
 </template>
 
-<script>
-import { mapWritableState } from "pinia";
+<script setup>
 import { useNotificationsStore } from "@/stores/useNotificationsStore";
-
+import { onClickOutside } from "@vueuse/core";
+import { ref } from "vue";
 import axios from "@/config/axios/index";
 import NotificationUnread from "@/components/notifications/NotificationUnread.vue";
 import NotificationRead from "@/components/notifications/NotificationRead.vue";
 
-export default {
-  components: {
-    NotificationUnread,
-    NotificationRead,
-  },
-  computed: {
-    ...mapWritableState(useNotificationsStore, [
-      "read",
-      "unread",
-      "unreadCount",
-    ]),
-  },
+const notification = useNotificationsStore();
 
-  methods: {
-    readAllNotification() {
-      axios.post("markAllAsRead").then(() => {
-        this.read.concat(this.unread);
-        this.unread.splice(0, this.unread.length);
-        this.unreadCount = 0;
-      });
-    },
-  },
+const notificationRef = ref(true);
+
+onClickOutside(notificationRef, () => {
+  notificationRef.value = false;
+});
+
+const readAllNotification = () => {
+  axios.post("markAllAsRead").then(() => {
+    this.read.concat(this.unread);
+    this.unread.splice(0, this.unread.length);
+    this.unreadCount = 0;
+  });
 };
 </script>
